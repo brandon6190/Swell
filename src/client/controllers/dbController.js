014620f63130197b3e3c8e7678010fbaa2ac5845
brandon6190 <brandon6190@outlook.com>
2019-01-05 19:36:28 -0500

    fixed formatting issues

--- src/client/controllers/dbController.js
+++ src/client/controllers/dbController.js
@@ -1,46 +1,45 @@
+import format from 'date-fns/format';
 import * as store from '../store';
 import * as actions from '../actions/actions';
 import db from '../db';
-import format from 'date-fns/format';
 
 const dbController = {
-
-  addToIndexDb (reqRes) {
-    db.history.put(reqRes)
+  addToIndexDb(reqRes) {
+    db.history
+      .put(reqRes)
       .then(() => {})
-      .catch((err) => console.log('Error in addToHistory', err))
-
+      .catch(err => console.log('Error in addToHistory', err));
   },
 
-  deleteFromIndexDb (id) {
-    db.history.delete(id)
+  deleteFromIndexDb(id) {
+    db.history
+      .delete(id)
       .then(() => {})
-      .catch((err) => console.log('Error in deleteFromHistory', err))
+      .catch(err => console.log('Error in deleteFromHistory', err));
   },
 
-  getHistory () { 
+  getHistory() {
     db.table('history')
       .toArray()
-      .then(history => { 
+      .then((history) => {
         const historyGroupsObj = history.reduce((groups, hist) => {
-          const date = format(hist.created_at, 'MM/DD/YYYY')
+          const date = format(hist.created_at, 'MM/DD/YYYY');
           if (!groups[date]) {
             groups[date] = [];
           }
           groups[date].push(hist);
           return groups;
         }, {});
-        const historyGroupsArr = Object.keys(historyGroupsObj).sort((a, b) => a - b).map(date => {
-          return {
-            date: date,
-            history: historyGroupsObj[date].sort((a, b) => b.created_at - a.created_at)
-          };
-        });
+        const historyGroupsArr = Object.keys(historyGroupsObj)
+          .sort((a, b) => a - b)
+          .map(date => ({
+            date,
+            history: historyGroupsObj[date].sort((a, b) => b.created_at - a.created_at),
+          }));
         store.default.dispatch(actions.getHistory(historyGroupsArr));
       })
       .catch(err => console.log('Error in getHistory', err));
-  }
-
-}
+  },
+};
 
 export default dbController;
